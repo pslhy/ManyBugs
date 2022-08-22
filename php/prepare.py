81e316a701f27ccb6ca7bc97b8112a1740d61d62
Jasper Lee <huna3869@hanyang.ac.kr>
2022-08-23 04:52:10 +0900

    fix: php build error fix by adding nasty version

--- php/prepare.py
+++ php/prepare.py
@@ -19,7 +19,9 @@ NASTY_REVISIONS = [
     "032d140fd6",
     "c1322d2505",
     "f5a9e17f9c",
-    "ff63c09e6f"
+    "ff63c09e6f",
+    "8138f7de40",
+    "b60f6774dc"
 ]
 
 # keep a list of test directories
